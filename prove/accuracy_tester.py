f1b4a965fcb568823efffc741dd02ca896d399f0
Alex <genovese.2108670@studenti.uniroma1.it>
2026-04-04 15:50:07 +0200

    Aggiunto accuracy tester

---
+++ prove/accuracy_tester.py
@@ -0,0 +1,27 @@
+title = "charles_darwin"
+parsed_mk = f"{title}.md"
+gs_path = f"..\progetto\supporto_temp\{title}_gs.txt"
+
+token_estratti = set()
+token_gs = set()
+
+with open(parsed_mk,encoding="utf-8") as f:
+    for line in f:
+        words = line.lower().strip().split()
+        for word in words:
+            token_estratti.add(word)
+
+with open(gs_path,encoding="utf-8") as f:
+    for line in f:
+        words = line.lower().strip().split()
+        for word in words:
+            token_gs.add(word)
+
+precision = len(token_estratti & token_gs)/len(token_estratti)
+recall = len(token_estratti & token_gs)/len(token_gs)
+f1 = (2*precision*recall)/(precision+recall)
+
+print(f"Risultati per {title}:")
+print("Precision: " + str(precision))
+print("Recall: " + str(recall))
+print("F1: " + str(f1))
