8733fff4cbea13b1b17cc7b58b94f3a5bc189ad0
Ludovica <ludofiocchetta@gmail.com>
2026-04-07 18:09:38 +0200

    aggiunte metriche

--- prove/accuracy_tester.py
+++ prove/accuracy_tester.py
@@ -1,3 +1,5 @@
+import math
+
 title = "scooby-doo"
 parsed_mk = f"{title}.md"
 gs_path = f"../progetto/supporto_temp/{title}_gs.txt"
@@ -19,15 +21,21 @@ with open(gs_path,encoding="utf-8") as f:
 
 intersection = token_estratti & token_gs
 union = token_estratti | token_gs
+den_cosine=math.sqrt(len(token_estratti))*math.sqrt(len(token_gs))
 
 
 precision = len(intersection)/len(token_estratti)
 recall = len(intersection)/len(token_gs)
 f1 = (2*precision*recall)/(precision+recall)
 jaccard_similarity = len(intersection)/len(union)
+overlap_coefficient=len(intersection)/min(len(token_estratti),len(token_gs))
+cosine_similarity=len(intersection)/den_cosine
+
 
 print(f"Risultati per {title}:")
 print("Precision: " + str(precision))
 print("Recall: " + str(recall))
 print("F1: " + str(f1))
 print("Jaccard Similarity: " + str(jaccard_similarity))
+print("Overlap Coefficient: "+str(overlap_coefficient))
+print("Cosine Similarity: "+str(cosine_similarity))
